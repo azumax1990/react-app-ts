1434194894474c4fcaeb456892bb75285c06f914
谷村有寿麻 <t.azumax0502@gmail.com>
2021-07-25 00:41:00 +0900

    ログイン時のメッセージ表示

--- src/apis/useAuth.ts
+++ src/apis/useAuth.ts
@@ -1,24 +1,28 @@
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router";
 import axios from "axios";
+
 import { User } from "../types/apis/user"; 
+import { useMessage } from "../hooks/useMessage";
 
 export const useAuth = () => {
 
   const history = useHistory();
   const [loading, setLoading] = useState(false);
+  const { showMessage } = useMessage();
 
   const login = useCallback((id: string) => {
     setLoading(true)
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then((res) => {
       if (res.data) {
+        showMessage({ title: "ログインしました", status: "success"});
         history.push("/home");
       } else {
-        alert("いないよ");
+        showMessage({ title: "ユーザーが見つかりません!!!", status: "error"});
       }
     })
-    .catch(() => alert("ログイン出来ないよ"))
+    .catch(() => showMessage({ title: "ユーザーが見つかりません", status: "error"}))
     .finally(() => setLoading(false));
   }, [])
   return { login, loading }
